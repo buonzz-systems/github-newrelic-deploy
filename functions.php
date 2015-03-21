cd997e05edae73bd40568a9b27330407a78ce89d
buonzz <buonzz@gmail.com>
2015-03-21 23:49:48 +0800

    v

--- functions.php
+++ functions.php
@@ -2,12 +2,13 @@
 
 
 function log_github_postdata(){
+
 	$gh_log_file = 'logs/github.log';
 	
 	if(isset($_POST))
 	{
 		$gh_data = parse_gh_data();
-		$log = "\r". date("Y-m-d H:i:s - ") . $gh_data['repo'] . ' - ' . $gh_data['user'];
+		$log = "\r". date("Y-m-d H:i:s - ") . $gh_data['repo'] . ' - ' . $gh_data['user'] . ' - ' . $gh_data['rev'];
 		file_put_contents($gh_log_file, $log , FILE_APPEND | LOCK_EX);
 
 	}
@@ -17,5 +18,6 @@ function parse_gh_data(){
 	$raw_data = json_decode($_POST['payload']);
 	$new_data['repo'] = $raw_data->repository->url;
 	$new_data['user'] = $raw_data->pusher->name;
+	$new_data['rev'] = $raw_data->rev;
 	return $new_data;
 }
