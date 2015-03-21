6e06a35ee9fa5042315c8839584d96d0e3e5750f
buonzz <buonzz@gmail.com>
2015-03-21 22:15:04 +0800

    fix

--- functions.php
+++ functions.php
@@ -7,14 +7,14 @@ function log_github_postdata(){
 	if(isset($_POST))
 	{
 		$gh_data = parse_gh_data($_POST);
-
-		file_put_contents($gh_log_file, $data['repo_url'], FILE_APPEND | LOCK_EX);
+		$log = date("\"Y-m-d: ". $data['repo_url']);
+		file_put_contents($gh_log_file, $log , FILE_APPEND | LOCK_EX);
 
 	}
 }
 
 function parse_gh_data($data){
 	$raw_data = json_decode($data);
-	$new_data['repo_url'] = $raw_data['repository']['url'];
+	$new_data['repo_url'] = $raw_data['repository']->url;
 	return $new_data;
 }
