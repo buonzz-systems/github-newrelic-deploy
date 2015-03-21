4105f87e16f1ceced10c1ec228ae1fff422be34b
buonzz <buonzz@gmail.com>
2015-03-21 20:35:35 +0800

    externalize config

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$apikey = "{INSERT YOUR API KEY HERE}";
+ 
+#Specify an existing New Relic app name OR app ID
+#$app_name = "{INSERT YOUR APPLICATION NAME HERE}";
+$app_id = "{INSERT YOUR APPLICATION ID HERE}";
+ 
+$dep_description = "This is your app id deployment";
