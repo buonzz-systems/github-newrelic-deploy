f8607d81b3988a05f9cff1f935bb2fe489a95c4d
buonzz <buonzz@gmail.com>
2015-03-22 15:09:06 +0800

    made multiple gh repo possible

--- config.sample.php
+++ config.sample.php
@@ -1,10 +1,16 @@
 <?php
 
 
+# specificy api key in here, read https://docs.newrelic.com/docs/apm/apis/requirements/api-key
+# on how to get an api key
 $apikey = "{INSERT YOUR API KEY HERE}";
  
-#Specify an existing New Relic app name OR app ID
-#$app_name = "{INSERT YOUR APPLICATION NAME HERE}";
-$app_id = "{INSERT YOUR APPLICATION ID HERE}";
+#Specify an existing New Relic app ID to Github Repos Mapping in here
+$app_ids[] = array( 'https://github.com/vendor/repo' 
+						=> "{INSERT YOUR APPLICATION ID HERE}");
  
+#detect the app id
+$gh_data = parse_gh_data();
+$app_id = $app_ids[$gh_data['repo']];
+
 $dep_description = "This is your app id deployment";
